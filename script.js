954a28c8cc67bf85930ef3289a599434bd0f0efd
DeepeshMadane <156791605+DeepeshMadane@users.noreply.github.com>
2024-04-05 16:38:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let menu=document.querySelector("#menu-bars");
-let navbar=document.querySelector(".navbar");
+
+
+// Toggle navbar menu
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-time');
+    menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+const searchIcon = document.getElementById('search-icon');
+const searchContainer = document.querySelector('.search-container');
+
+// Toggle search container on click of search icon
+searchIcon.addEventListener('click', () => {
+  searchContainer.style.display = searchContainer.style.display === 'block' ? 'none' : 'block';
+});
